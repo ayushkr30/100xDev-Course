9b7ce9e4334fa05841bdaf782824ecfae8f9ca7d
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-05-10 23:46:22 +0530

    changes

--- Week-1/Practise-3.js
+++ Week-1/Practise-3.js
@@ -8,58 +8,7 @@ class Animal {
     }
   }
 
-  function dateMethods() {
-    const currentDate = new Date();
-    console.log("Current Date:", currentDate);
   
-    // Getting various components of the date
-    console.log("Date:", currentDate.getDate());
-    console.log("Month:", currentDate.getMonth() + 1); // Months are zero-indexed, so adding 1
-    console.log("Year:", currentDate.getFullYear());
-    console.log("Hours:", currentDate.getHours());
-    console.log("Minutes:", currentDate.getMinutes());
-    console.log("Seconds:", currentDate.getSeconds());
-  
-    // Setting components of the date
-    currentDate.setFullYear(2022);
-    console.log("After setFullYear:", currentDate);
-  
-    currentDate.setMonth(5); // Setting month to June (zero-indexed)
-    console.log("After setMonth:", currentDate);
-  
-    // Getting and setting time in milliseconds since 1970
-    console.log("Time in milliseconds since 1970:", currentDate.getTime());
-  
-    const newDate = new Date(2023, 8, 15); // Creating a new date
-    console.log("New Date:", newDate);
-  }
-  
-  // Example Usage for Date Methods
-  dateMethods();
-
-
-  //JSon
-
-  function jsonMethods(jsonString) {
-    console.log("Original JSON String:", jsonString);
-  
-    // Parsing JSON string to JavaScript object
-    let parsedObject = JSON.parse(jsonString);
-    console.log("After JSON.parse():", parsedObject);
-  
-    // Stringifying JavaScript object to JSON string
-    let jsonStringified = JSON.stringify(parsedObject);
-    console.log("After JSON.stringify():", jsonStringified);
-  }
-  
-  // Example Usage for JSON Methods
-  const sampleJSONString =
-    '{"key": "value", "number": 42, "nested": {"nestedKey": "nestedValue"}}';
-  
-  jsonMethods(sampleJSONString);
-
-  //Math Class
-
   function mathMethods(value) {
     console.log("Original Value:", value);
   
