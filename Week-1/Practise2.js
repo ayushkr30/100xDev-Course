050b758a2dbc6147946224a1200a3e3e938f0c68
Ayush Kumar <ayushpal2017ak@gmail.com>
2024-05-12 22:50:15 +0530

    Update Practise2.js

--- Week-1/Practise2.js
+++ Week-1/Practise2.js
@@ -6,7 +6,24 @@
 // Run each function to see the output, play and learn by doing.
 
 // Length
+function getLength(str) {
+  console.log("Original String:", str);
+  console.log("Length:", str.length);
+}
+getLength("Hello World");
 
+// indexOf
+function findIndexOf(str, target) {
+  console.log("Original String:", str);
+  console.log("Index:", str.indexOf(target));
+}
+findIndexOf("Hello World", "World");
+
+// lastIndexOf
+function findLastIndexOf(str, target) {
+  console.log("Original String:", str);
+  console.log("Index:", str.lastIndexOf(target));
+}
   findLastIndexOf("Hello World World", "World");
   
   // slice
